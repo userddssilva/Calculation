bfaa3da8b1f34c601a522ca7c705d1d940fafb58
Dayvson Silva <dayvson.silva@samsung.com>
2022-02-25 14:05:18 -0400

    start project and create calculate class

---
+++ Calculation/Calculation/Class1.cs
@@ -0,0 +1,20 @@
+﻿namespace Calculation
+{
+    /// <summary>
+    ///  Class used for calculation purpose like addition and subtraction
+    /// </summary>
+    public class calculate
+    {
+        // Method used for Addition
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        // Method used for Subtraction
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
